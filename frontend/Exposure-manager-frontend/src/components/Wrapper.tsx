5f0a1796d513d2e585b21bae362d9bc78beeb81b
Frogo-o <ralchev.gg@gmail.com>
2025-07-04 16:22:54 +0300

    fixed type import

--- frontend/Exposure-manager-frontend/src/components/Wrapper.tsx
+++ frontend/Exposure-manager-frontend/src/components/Wrapper.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, useState, useRef, useEffect } from 'react';
+import {  useState, useRef, useEffect } from 'react';
+import type {ReactNode} from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 import '../styles/Wrapper.css';
 import ThemeToggle from './ThemeToggle';

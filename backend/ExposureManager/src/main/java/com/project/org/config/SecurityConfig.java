8f8f1f75df772ffbf8d423caa22a0535aa5ef180
Alexander Dyankov <alexander.dyankov@gmail.com>
2025-07-15 14:48:34 +0300

    Configured the cors policy for the exposure manager

--- backend/ExposureManager/src/main/java/com/project/org/config/SecurityConfig.java
+++ backend/ExposureManager/src/main/java/com/project/org/config/SecurityConfig.java
@@ -4,9 +4,13 @@ import com.project.org.security.filter.jwt.JwtVerificationFilter;
 import com.project.org.service.CustomUserDetailsService;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -18,9 +22,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
@@ -28,8 +37,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http,
                                                    JwtVerificationFilter jwtVerificationFilter,
+                                                   CorsConfigurationSource corsConfigurationSource,
                                                    AuthenticationProvider authProvider) throws Exception {
         http.csrf(customizer -> customizer.disable())
+                .cors(customizer -> customizer.configurationSource(corsConfigurationSource))
                 .authorizeHttpRequests(request ->
                         request.requestMatchers("/login", "/signup").permitAll()
                                 .anyRequest().authenticated())
@@ -37,6 +48,19 @@ public class SecurityConfig {
                 .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterAfter(jwtVerificationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    @Primary
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean

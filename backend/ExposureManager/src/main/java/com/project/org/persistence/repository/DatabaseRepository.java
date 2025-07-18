2fc3f4d6bf9a51cfe4fdfbae3af67ce76f2992e0
Alexander Dyankov <alexander.dyankov@gmail.com>
2025-07-18 14:38:18 +0300

    Added @Repository to the repos

--- backend/ExposureManager/src/main/java/com/project/org/persistence/repository/DatabaseRepository.java
+++ backend/ExposureManager/src/main/java/com/project/org/persistence/repository/DatabaseRepository.java
@@ -4,9 +4,11 @@ import com.project.org.persistence.entity.DatabaseEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface DatabaseRepository extends JpaRepository<DatabaseEntity, Long> {
 
     Page<DatabaseEntity> findAllByOwner_Username(String ownerUsername, Pageable pageable);

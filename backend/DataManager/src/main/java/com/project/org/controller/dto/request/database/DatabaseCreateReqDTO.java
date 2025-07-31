7d03a626977e0fa55e62739e6b2e4bf802068cde
Alexander Dyankov <alexander.dyankov@gmail.com>
2025-07-31 10:15:15 +0300

    Datbase req dtos now implement ReqDTO

--- backend/DataManager/src/main/java/com/project/org/controller/dto/request/database/DatabaseCreateReqDTO.java
+++ backend/DataManager/src/main/java/com/project/org/controller/dto/request/database/DatabaseCreateReqDTO.java
@@ -1,8 +1,9 @@
 package com.project.org.controller.dto.request.database;
 
+import com.project.org.controller.dto.request.ReqDTO;
 import lombok.Data;
 
 @Data
-public class DatabaseCreateReqDTO {
+public class DatabaseCreateReqDTO implements ReqDTO {
     private String name;
 }
--- backend/DataManager/src/main/java/com/project/org/controller/dto/request/database/DatabaseRenameReqDTO.java
+++ backend/DataManager/src/main/java/com/project/org/controller/dto/request/database/DatabaseRenameReqDTO.java
@@ -1,9 +1,10 @@
 package com.project.org.controller.dto.request.database;
 
+import com.project.org.controller.dto.request.ReqDTO;
 import lombok.Data;
 
 @Data
-public class DatabaseRenameReqDTO {
+public class DatabaseRenameReqDTO implements ReqDTO {
     private String oldName;
     private String newName;
 }
